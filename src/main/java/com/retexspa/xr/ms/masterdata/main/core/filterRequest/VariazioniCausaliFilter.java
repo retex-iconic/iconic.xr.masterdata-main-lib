4bca9c32a9712089a0da70c4ebd398a71255d9d1
RETEX\l.turchetto <luisa.turchetto@3esse.retexspa.com>
2024-07-17 13:11:24 +0200

    ri commit

--- src/main/java/com/retexspa/xr/ms/masterdata/main/core/filterRequest/VariazioniCausaliFilter.java
+++ src/main/java/com/retexspa/xr/ms/masterdata/main/core/filterRequest/VariazioniCausaliFilter.java
@@ -6,7 +6,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.time.LocalDateTime;
 import java.util.LinkedHashMap;
-import java.util.Objects;
 
 public class VariazioniCausaliFilter {
 
