e199f6b9c87cb5b7b024f9e383d1f30d9e7bc55c
RETEX\l.turchetto <luisa.turchetto@3esse.retexspa.com>
2024-07-30 16:08:12 +0200

    refactoring Tassonomia -2-

--- src/main/java/com/retexspa/xr/ms/masterdata/main/query/repositories/TassonomiaRepository.java
+++ src/main/java/com/retexspa/xr/ms/masterdata/main/query/repositories/TassonomiaRepository.java
@@ -2,18 +2,32 @@ package com.retexspa.xr.ms.masterdata.main.query.repositories;
 
 import com.retexspa.xr.ms.masterdata.main.query.entities.TassonomiaQueryEntity;
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface TassonomiaRepository
-    extends JpaRepository<TassonomiaQueryEntity, String>,
-        JpaSpecificationExecutor<TassonomiaQueryEntity> {
+                extends JpaRepository<TassonomiaQueryEntity, String>,
+                JpaSpecificationExecutor<TassonomiaQueryEntity> {
 
-  List<TassonomiaQueryEntity> findAllByCodice(String codice);
+        List<TassonomiaQueryEntity> findAllByCodice(String codice);
 
-  List<TassonomiaQueryEntity> findAllByCodiceAndTipoTassonomiaId(
-      String codice, String tipoTassonomiaId);
+        List<TassonomiaQueryEntity> findAllByTipoTassonomiaId(String tipoTassonomiaId);
 
-  List<TassonomiaQueryEntity> findAllByCodiceAndTipoTassonomiaIdAndNodoId(
-      String codice, String tipoTassonomiaId, String nodoId);
+        List<TassonomiaQueryEntity> findAllByCodiceAndTipoTassonomiaId(
+                        String codice, String tipoTassonomiaId);
+
+        List<TassonomiaQueryEntity> findAllByCodiceAndTipoTassonomiaIdAndNodoId(
+                        String codice, String tipoTassonomiaId, String nodoId);
+
+        List<TassonomiaQueryEntity> findAllById(
+                        String tassonomiaId);
+
+        Optional<TassonomiaQueryEntity> findById(String id);
+
+        List<TassonomiaQueryEntity> findAllByPadreId(String id);
+
+        List<TassonomiaQueryEntity> findAllByNodoId(
+                        String id);
 }
